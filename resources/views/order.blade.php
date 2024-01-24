0ed1ed1bfc69be3082cb3eb14160765c999187c3
kruizo <kynethruizo@gmail.com>
2024-01-24 08:15:43 +0800

    added views

---
+++ resources/views/order.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title')
+<title>Order</title>
+@endsection
+
+
+@section('content')
+<section id="order-form">
+    <div class="pt-24">
+
+    </div>
+</section>
+
+@endsection
+
+@section('modal')
+@include('modals.authentication')
+@endsection
