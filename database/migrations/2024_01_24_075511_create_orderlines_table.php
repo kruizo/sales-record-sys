a1e3ae85a2039a532d4dd67ddc01a255996468d1
kruizo <kynethruizo@gmail.com>
2024-01-24 16:47:47 +0800

    added some db tables

---
+++ database/migrations/2024_01_24_075511_create_orderlines_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderlines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('water_id')->constrained('waters')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderlines');
+    }
+};
