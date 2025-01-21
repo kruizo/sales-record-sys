caf7d42401b7a0d1098a955861d7e908ae3f0ed7
kruizo <kynethruizo@gmail.com>
2025-01-22 05:03:24 +0800

    fixed vite issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/css/app.css', 'resources/js/app.js'], ['version' => '1.0'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
