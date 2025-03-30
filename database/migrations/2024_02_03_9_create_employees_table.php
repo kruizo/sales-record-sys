1d36f02f4c255015d00ce4b55aaf4b1e15fee312
kruizo <kynethruizo@gmail.com>
2025-03-30 15:30:24 +0800

    tables: removed unused tables

--- database/migrations/2024_02_03_9_create_employees_table.php
+++ database/migrations/2024_02_03_9_create_employees_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('lastname');
             $table->string('contactnum');
             $table->string('email');
-            $table->boolean('is_archived');
+            $table->boolean('is_archived')->default(0);
             $table->timestamps();
         });
     }
